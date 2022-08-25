6bf15ba1fff63487a15673c06cf7306516866644
TheRankN1 <petrutpoenaru@yahoo.com>
2022-08-25 19:36:30 +0300

    Revert "add auth"
    This reverts commit 5e37782c

--- api/modules/user/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const UserController = require('./user.controller');
-
-router.get('/getUsers', UserController.getUsers);
-router.get('/getUserById/:id' , UserController.getUserById)
-router.delete('/deleteUsers/:id', UserController.deleteUsers);
-module.exports = router;
